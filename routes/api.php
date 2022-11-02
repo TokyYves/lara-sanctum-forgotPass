85f7926c5b79717c453adb8eeacc01235c30644d
Newt <tokyyves.nosytech@gmail.com>
2022-11-02 12:26:18 +0300

    first commit

--- routes/api.php
+++ routes/api.php
@@ -8,28 +8,18 @@ use App\Http\Controllers\Auth\ResetPassController;
 use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\CommentController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('guest')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/forgot', [ResetPassController::class, 'forgot']);
     Route::post('/reset', [ResetPassController::class, 'reset']);
+    Route::get('/posts', [PostController::class,'index']);
 });
 Route::middleware('auth:sanctum', 'verified')->group(function () {
     Route::get('/authenticated', [AuthController::class, 'authenticated']);
     Route::get('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('/posts', PostController::class);
     Route::apiResource('/comments', CommentController::class);
+    Route::apiResource('/posts', PostController::class)->except('index');
 });
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/email/check', [EmailVerificationController::class, 'check']);
